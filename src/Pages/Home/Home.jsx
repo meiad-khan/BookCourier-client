52aae585ba4174233c2859cb57f87948df2a6eb3
meiad-khan <meiadkhan615@gmail.com>
2026-02-12 14:22:09 +0600

    added coverage section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Banner from './Banner';
+import LatestBook from './LatestBook';
+import Coverage from './Coverage';
+import Loading from '../../Components/Loading/Loading';
+
+ const coveragePromise =  fetch("/serviceCenters.json").then((res) => res.json());
 
 const Home = () => {
   return (
     <div>
-      <div className='mt-8'>
+      <div className="mt-8">
         <Banner></Banner>
+      </div>
+      <div>
+        <LatestBook></LatestBook>
+      </div>
+      <div>
+        <Suspense fallback={<Loading></Loading>}>
+          <Coverage coveragePromise={coveragePromise}></Coverage>
+        </Suspense>
       </div>
     </div>
   );
---
+++ src/Pages/Home/LatestBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LatestBook = () => {
+  return (
+    <div>
+      <h4 className="text-4xl font-bold text-center">Latest <span className='text-primary'>Books</span></h4>
+    </div>
+  );
+};
+
+export default LatestBook;
